9d72b81b9334e97cdd7982493601587f273bd8f4
Lenya Veraksa <lenyaz@outlook.com>
2020-09-09 00:20:22 +0300

    little renaming

--- extension.js
+++ extension.js
@@ -1,19 +1,19 @@
 const ExtensionUtils = imports.misc.extensionUtils;
-const kPower = ExtensionUtils.getCurrentExtension().imports.kPower;
-const Log = kPower.Log;
+const iPower = ExtensionUtils.getCurrentExtension().imports.iPower;
+const Log = iPower.Log;
 const Main = imports.ui.main;
 
 var btKeybBattIndicator;
 function enable() {
 	Log("enable");
-	btKeybBattIndicator = new kPower.kBattIndicator();
+	btKeybBattIndicator = new iPower.kBattIndicator();
 	Main.panel.addToStatusArea('BtKeybBattIndicator', btKeybBattIndicator, 1);
 }
 
 function disable() {
 	Log("Disable");
-	kPower.dbusCon.signal_unsubscribe(btKeybBattIndicator.subIdAdd);
-	kPower.dbusCon.signal_unsubscribe(btKeybBattIndicator.subIdRem);
+	iPower.dbusCon.signal_unsubscribe(btKeybBattIndicator.subIdAdd);
+	iPower.dbusCon.signal_unsubscribe(btKeybBattIndicator.subIdRem);
 	btKeybBattIndicator._proxy = null;
 	btKeybBattIndicator.reset();
 	btKeybBattIndicator.destroy();
